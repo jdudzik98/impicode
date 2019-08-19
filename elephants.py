428efbc9ef7e32556bb829692472be59f7529813
jandudzik <dudzikjanek@gmail.com>
2019-08-20 00:20:27 +0200

    Changing two lists of data into permutation in form of one list

--- elephants.py
+++ elephants.py
@@ -9,3 +9,11 @@ lines = sys.stdin.readlines()
 for single_line in lines:
     single_line = single_line.replace('\n', '').split(" ")
     data.append(single_line)
+
+"""Turning data into permutation list"""
+data[4] = []
+
+for elephants in range(1, int(data[0][0]) + 1):
+
+    data[4].append(data[2][int(data[3].index(str(elephants)))])
+del data[2], data[3]
