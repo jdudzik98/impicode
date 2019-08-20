a251c94c4b111529483595eb83ac50668516104d
jandudzik <dudzikjanek@gmail.com>
2019-08-21 01:26:34 +0200

    Create list of single cycles (actually list of lists-cycles)

--- elephants.py
+++ elephants.py
@@ -16,6 +16,25 @@ data.append([])
 
 for elephant in range(1, int(data[0][0]) + 1):
 
-    data[4].append(int(data[2].index(data[3][elephant-1])+1))
+    data[4].append(data[2].index(data[3][elephant-1]) + 1)
 
 del data[3], data[2]
+
+"""Turning permutation into single cycles"""
+
+data.append([False]*int(data[0][0]))
+
+cycles = []
+
+c = 0
+
+for i in range(len(data[3])):
+    if not data[3][i]:
+        c += 1
+        x = i + 1
+        while not data[3][x-1]:
+            data[3][x-1] = True
+            if len(cycles) != c:
+                cycles.append([])
+            cycles[int(c)-1].append(x)
+            x = data[2][int(x)-1]
